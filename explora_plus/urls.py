5fb32f7bca13b325ab51f88ddd35efac18ea98d8
Lucas Carmona <lucascarmonaneto510@gmail.com>
2026-05-26 21:55:27 -0300

    register apps and wire urls

--- explora_plus/urls.py
+++ explora_plus/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("core.urls")),
+    path("api/", include("accounts.urls")),
+    path("api/", include("places.urls")),
+    path("api/", include("routes.urls")),
+    path("api/", include("tickets.urls")),
 ]
